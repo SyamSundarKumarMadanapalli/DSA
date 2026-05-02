d63529019d49d4b8c0e77a7455a8b666cfb3cac6
Madanapalli Syam Sundar Kumar <138554859+SyamSundarKumarMadanapalli@users.noreply.github.com>
2026-05-02 09:08:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -3,19 +3,17 @@ class Solution {
         int sum = 0;
         int n = nums.length;
         double maxAvg = Integer.MIN_VALUE;
+
         for (int i = 0; i < k; i++){
             sum += nums[i];
         }
-        System.out.println(sum);
         maxAvg = (double)sum / k;
-        System.out.println(maxAvg);
 
         for(int i = k; i < n; i++){
             sum -= nums[i - k];
             sum += nums[i];
             double avg = (double)sum / k;
             maxAvg = Math.max(maxAvg, avg);
-             System.out.println(maxAvg);
         }
 
         return maxAvg;
