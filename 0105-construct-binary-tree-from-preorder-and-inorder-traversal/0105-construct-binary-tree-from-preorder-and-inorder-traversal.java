e4b671c7a7fc4a053a981cda6b10248302efa3bc
Madanapalli Syam Sundar Kumar <138554859+SyamSundarKumarMadanapalli@users.noreply.github.com>
2026-05-09 11:04:22 +0530

    Time: 81 ms (6.19%), Space: 46 MB (73.31%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,46 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    private TreeNode constructTree(int[] preorder, int preStart, int preEnd, int[] inorder, int inStart, int inEnd, Map<Integer, Integer> mp){
-        if(preStart > preEnd || inStart > inEnd){
-            return null;
-        }
 
-        TreeNode root = new TreeNode(preorder[preStart]);
+    int preIndex = 0;
+    Map<Integer, Integer> map = new HashMap<>();
 
-        int eleIndex = mp.get(root.val);
-        int noEle = eleIndex - inStart;
-        
-        root.left = constructTree(preorder, preStart + 1, preStart + noEle, inorder, inStart, eleIndex - 1, mp);
-        root.right = constructTree(preorder, preStart + noEle + 1, preEnd, inorder, eleIndex + 1, inEnd, mp);
+    public TreeNode build(int[] preorder, int left, int right) {
+        if (left > right) return null;
+
+        int rootVal = preorder[preIndex++];
+        TreeNode root = new TreeNode(rootVal);
+
+        int mid = map.get(rootVal);
+
+        root.left = build(preorder, left, mid - 1);
+        root.right = build(preorder, mid + 1, right);
 
         return root;
     }
 
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        int preStart = 0;
-        int preEnd = preorder.length - 1;
-        int inStart = 0; 
-        int inEnd = inorder.length - 1;
 
-        Map<Integer, Integer> mp = new HashMap<>();
-        for(int i = 0; i <= inEnd; i++){
-            mp.put(inorder[i], i);
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
         }
 
-        return constructTree(preorder, preStart, preEnd, inorder, inStart, inEnd, mp);
+        return build(preorder, 0, inorder.length - 1);
     }
 }
