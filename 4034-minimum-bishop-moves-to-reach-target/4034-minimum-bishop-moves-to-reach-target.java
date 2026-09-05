8f3f2627015ba4bba42a992a277e9a88d4ce3bfd
Madanapalli Syam Sundar Kumar <138554859+SyamSundarKumarMadanapalli@users.noreply.github.com>
2026-09-05 06:32:50 +0530

    Time: 1 ms (95.02%), Space: 43.9 MB (85.13%) - LeetHub

--- 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
+++ 4034-minimum-bishop-moves-to-reach-target/4034-minimum-bishop-moves-to-reach-target.java
@@ -4,22 +4,31 @@ class Solution {
         int scCol = source[1];
         int tarRow = target[0];
         int tarCol = target[1];
-System.out.println(tarRow - tarCol == scRow - scCol);
-        if((scRow % 2 == 0 && scCol % 2 == 0) || (scRow % 2 != 0 && scCol % 2 != 0)){
-            if((tarRow % 2 == 0 && tarCol % 2 == 0) || (tarRow % 2 != 0 && tarCol % 2 != 0)){
-                if((tarRow + tarCol == scRow + scCol) || (tarRow - tarCol == scRow - scCol)) return 1;
-                else return 2;
-            }
-            else return -1;
+
+        // if((scRow % 2 == 0 && scCol % 2 == 0) || (scRow % 2 != 0 && scCol % 2 != 0)){
+        //     if((tarRow % 2 == 0 && tarCol % 2 == 0) || (tarRow % 2 != 0 && tarCol % 2 != 0)){
+        //         if((tarRow + tarCol == scRow + scCol) || (tarRow - tarCol == scRow - scCol)) return 1;
+        //         else return 2;
+        //     }
+        //     else return -1;
+        // }
+        // else{
+        //     if((tarRow % 2 == 0 && tarCol % 2 == 0) || (tarRow % 2 != 0 && tarCol % 2 != 0)){
+        //         return -1;
+        //     }
+        //     else{
+        //         if((tarRow + tarCol == scRow + scCol) || (tarRow - tarCol == scRow - scCol)) return 1;
+        //         else return 2;
+        //     }
+        // }
+
+        if(scRow==tarRow && scCol==tarCol)return 0;
+        if(Math.abs(scRow-tarRow)==Math.abs(scCol-tarCol)){
+            return 1;
         }
-        else{
-            if((tarRow % 2 == 0 && tarCol % 2 == 0) || (tarRow % 2 != 0 && tarCol % 2 != 0)){
-                return -1;
-            }
-            else{
-                if((tarRow + tarCol == scRow + scCol) || (tarRow - tarCol == scRow - scCol)) return 1;
-                else return 2;
-            }
+        if((scRow+scCol)%2!=(tarRow+tarCol)%2){
+            return -1;
         }
+        return 2;
     }
 }
